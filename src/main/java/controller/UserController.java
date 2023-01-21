4f89df084f1aa6002a5c95749edfff0ce133502e
Евгений <jekarundin@yandex.ru>
2023-01-21 13:05:44 +0600

    ff

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import model.User;
+import service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/")
+    public String allUser(ModelMap model) {
+        model.addAttribute("allUsers", userService.getUsers());
+        return "index";
+    }
+
+    @GetMapping(value = "/createUser")
+    public String addUser(ModelMap model) {
+        User person = new User();
+        model.addAttribute("person", person);
+        return "user";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveEmployee(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable(value = "id") long id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showFormForUpdate(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "update";
+    }
+
+    @RequestMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,14 @@
+package service;
+
+import model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void addUser(User user);
+    List<User> getUsers();
+    User getUserById(long id);
+    void updateUser(User user);
+    void deleteUser(long id);
+
+}
